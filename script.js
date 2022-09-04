5a15ff6cf1f4c1ff4e3d80ce96b0e0ce1ac1a74b
Hassaan Ahmed <ahassaan673@gmail.com>
2022-09-04 06:05:04 +0500

    test

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const compile = () => {
-    const css = document.getElementById("css");
     const html = document.getElementById("html");
+    const css = document.getElementById("css");
     const js = document.getElementById("js");
 
     const result = document.getElementById("result").contentWindow.document;
     document.body.onkeyup = function () {
-        
-        code.open();
-        code.writeIn(
-            "Hello"
+
+        result.open();
+        result.writeIn(
+            html.value
+            + "<style>" +
+            css.value + "</style>"
+            + "<script>" +
+            js.value +
+            "</result>"
         );
-
-
+        result.close();
     }
-
 }
 
 compile();
